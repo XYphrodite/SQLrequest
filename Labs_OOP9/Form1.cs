276b307cf0eddf118ab02a57bfbf76dfe6244933
Maksim <72796194+XYphrodite@users.noreply.github.com>
2022-07-11 14:02:17 +0300

    Update Form1.cs

--- Labs_OOP9/Form1.cs
+++ Labs_OOP9/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Labs_OOP9
 {
     public partial class Form1 : Form
     {
+        DataBase dataBase = new DataBase();
+        int selectedRows;
+        enum RowState
+        {
+            Existed,
+            New,
+            Modified,
+            ModifiedNew,
+            Deleted
+        }
+        private void CreateColumns()
+        {
+            dataGridView1.Columns.Add("ID", "ID");
+            dataGridView1.Columns.Add("Name", "Имя");
+            dataGridView1.Columns.Add("pet", "Питомец");
+            dataGridView1.Columns.Add("nameofillness", "Название болезни");
+            dataGridView1.Columns.Add("numberofattendences", "Количество посещениий");
+            dataGridView1.Columns.Add("IsNew", String.Empty);
+        }
+        private void ReadSingleRow(DataGridView dgv,IDataRecord record)
+        {
+            dgv.Rows.Add(record.GetInt32(0),record.GetString(1), record.GetString(2), record.GetString(3), record.GetInt32(4));
+        }
+        private void RefreshDataGrid(DataGridView dgv)
+        {
+            dgv.Rows.Clear();
+
+            string queryString = $"select * from Hospital";
+            SqlCommand command = new SqlCommand(queryString, dataBase.getConnection());
+            dataBase.openConnection();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ReadSingleRow(dgv,reader);
+            }
+            reader.Close();
+        }
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateColumns();
+            RefreshDataGrid(dataGridView1);
         }
     }
 }
